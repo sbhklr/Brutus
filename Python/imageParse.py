1a9bbd65e3b9a5c66801efb3f612c04977e25b29
Sebastian Hunkeler <hunkeler.sebastian@gmail.com>
2017-07-01 21:20:18 +0200

    Dither image

--- Python/imageParse.py
+++ Python/imageParse.py
@@ -1,30 +1,42 @@
 import base64, string
 import struct
 from PIL import Image # Using Pillow
+from PIL import ImageEnhance
 
-# logo_width  384 (176)
-# logo_height 153
+def change_contrast(img, level):
+    factor = (259 * (level + 255)) / (255 * (259 - level))
+    def contrast(c):
+        value = 128 + factor * (c - 128)
+        return max(0, min(255, value))
+    return img.point(contrast)
 
-spacing = 105
+max_width = 384
 imageWidth = 176
+spacing = (max_width - imageWidth) / 2
 imageHeight = 153
 
 # Returns 8 Bit Int Array
 def imageParse(fileName):
     parsedImageArray = []
-    im = Image.open(fileName)
-    im = im.resize((imageWidth,imageHeight), Image.ANTIALIAS)
+
+    original = Image.open(fileName)    
+    resized = original.resize((imageWidth,imageHeight), Image.ANTIALIAS)
+    #contrast_image = change_contrast(resized, 175)
+    dithered = resized.convert(mode='1')
+    #dithered.save("dithered.jpg")    
+
+    im = dithered    
     pix = im.load()
-    print im.size #Get the width and hight of the image for iterating over
-    (r,g,b) = pix[1,1]
+    print im.size #Get the width and hight of the image for iterating over    
     imgStr = ''
     currentBlock = '';
     for y in range(0, im.height):
         line = ''
         for x in range(0, im.width + spacing * 2):
             if(x > spacing and x < (im.width + spacing)):
-                (r,g,b) = pix[x-spacing,y]
-                if((r+b+g)/3 >= 128):
+                #(r,g,b) = pix[x-spacing,y]
+                #if((r+b+g)/3 >= 128):
+                if(pix[x-spacing,y] >= 128):
                     currentBlock += '0'
                 else:
                     currentBlock += '1'
