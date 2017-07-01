f22e317f3a990310b787e7840a2b59fbb8ed3cc0
Sebastian Hunkeler <hunkeler.sebastian@gmail.com>
2017-07-01 17:56:34 +0200

    Removed sendStatus from facesApi

--- Python/facesApi.py
+++ Python/facesApi.py
@@ -1,15 +1,8 @@
+#!/usr/bin/python
+
 import httplib, urllib, base64, json
 from threading import Timer
 import time
-
-def sendStatus(status):
-    try:
-        conn = httplib.HTTPConnection("192.168.1.121", 9080)
-        conn.request("GET", "/?display=" + urllib.quote(status))
-        response = conn.getresponse()
-        print response.status, response.reason
-    except Exception as e:
-        print("[Errno {0}] {1}".format(e, e))
 
 # Return format Makeup;Pyjama;Hipster;Youngsster;BadMood;Aggressive
 def getFaceData(imageContent):
