ffa646dc4abe63e962d3d27622b72797690687d9
Sebastian Hunkeler <hunkeler.sebastian@gmail.com>
2017-08-22 18:33:51 +0200

    Removed old code

--- Python/display.py
+++
@@ -1,8 +0,0 @@
-def sendStatus(status):
-    try:
-        conn = httplib.HTTPConnection("192.168.1.121", 9080)
-        conn.request("GET", "/?display=" + urllib.quote(status))
-        response = conn.getresponse()
-        print response.status, response.reason
-    except Exception as e:
-        print("[Errno {0}] {1}".format(e, e))
